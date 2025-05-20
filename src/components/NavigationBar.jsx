c647160b72b8a46db915518c6a13f514e679fa43
Harsha Buddhika <59781888+HarshaBuddhikaOfficial@users.noreply.github.com>
2025-05-20 16:53:00 +0530

    update files

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,14 +1,16 @@
+import '../styling.css'
+
 function NavigationBar(){
     return(
         <div className="flex justify-between items-center bg-gray-800 text-white p-4">
             <h1>Harsha Buddhika</h1>
             <nav className="flex gap-4 ">
-                <button className="cursor-pointer hover:underline">Home</button>
-                <button className="cursor-pointer hover:underline">about</button>
-                <button className="cursor-pointer hover:underline">Project</button>
-                <button className="cursor-pointer hover:underline">Skills</button>
-                <button className="cursor-pointer hover:underline">Education</button>
-                <button className="cursor-pointer hover:underline">Contacts</button>
+                <button className="navi">Home</button>
+                <button className="navi">about</button>
+                <button className="navi">Project</button>
+                <button className="navi">Skills</button>
+                <button className="navi">Education</button>
+                <button className="navi">Contacts</button>
             </nav>
         </div>
     );
